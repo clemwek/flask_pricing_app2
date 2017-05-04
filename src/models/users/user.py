52bd56fb7ae22e344766c8362f034af508a3ca48
Clement <clemwek>
2017-05-04 09:14:23 +0300

    Completed login and registration

--- src/models/users/user.py
+++ src/models/users/user.py
@@ -1,8 +1,59 @@
+import uuid
+
+from src.common.database import Database
+from src.common.utils import Utils
+import src.models.users.errors as UserErrors
+
+
 class User(object):
-    def __init__(self, email, password):
+    def __init__(self, email, password, _id=None):
         self.email = email
         self.password = password
+        self._id = uuid.uuid4().hex if _id is None else _id
 
     def __repr__(self):
         return '<User {}>'.format(self.email)
 
+    @staticmethod
+    def is_login_valid(email, password):
+        """
+        
+        :param email: 
+        :param password: 
+        :return: 
+        """
+        user_data = Database.find_one("users", {"email": email})
+        if user_data is None:
+            raise UserErrors.UserNotExistError('Your user does not exist')
+        if not Utils.check_hashed_password(password, user_data['password']):
+            raise UserErrors.IncorrectPasswordError('your password is not correct')
+        return True
+
+    @staticmethod
+    def register_user(email, password):
+        """
+        
+        :param email: 
+        :param password: 
+        :return: 
+        """
+        user_data = Database.find_one('users', {"email": email})
+
+        if user_data is not None:
+            raise UserErrors.UserAlreadyRegisteredError('Your email has already been used.')
+        if not Utils.email_is_valid(email):
+            raise UserErrors.InvalidEmailError('You are not using a valid email.')
+
+        User(email, Utils.hash_password(password)).save_to_db()
+
+        return True
+
+    def save_to_db(self):
+        Database.insert("users", self.json())
+
+    def json(self):
+        return {
+            "_id": self._id,
+            "email": self.email,
+            "password": self.password
+        }
--- src/models/users/views.py
+++ src/models/users/views.py
@@ -1,21 +1,45 @@
-from flask import Blueprint
+from flask import Blueprint, request, session, url_for, render_template
+from werkzeug.utils import redirect
+
+from src.models.users.user import User
+import src.models.users.errors as UserErrors
 
 user_blueprint = Blueprint('users', __name__)
 
 
-@user_blueprint.route('/login')
+@user_blueprint.route('/login', methods=['GET', 'POST'])
 def user_login():
-    pass
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['hashed']
+
+        try:
+            if User.is_login_valid(email, password):
+                session['email'] = email
+                return redirect(url_for('.user_alerts'))
+        except UserErrors.UserError as e:
+            return e.message
+    return render_template('users/loin.html')
 
 
-@user_blueprint.route('/register')
+@user_blueprint.route('/register', methods=['GET', 'POST'])
 def user_register():
-    pass
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['hashed']
+
+        try:
+            if User.register_user(email, password):
+                session['email'] = email
+                return redirect(url_for('.user_alerts'))
+        except UserErrors.UserError as e:
+            return e.message
+    return render_template('users/register.html')
 
 
 @user_blueprint.route('/alerts')
 def user_alerts():
-    pass
+    return 'We are getting here'
 
 
 @user_blueprint.route('/logout')
